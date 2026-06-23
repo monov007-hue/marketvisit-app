60527222f454276aa632ec68928011707afd30ca
monov007-hue <mono.v007@gmail.com>
2026-06-23 16:14:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -432,6 +432,8 @@ def main():
     async def run():
         await start_web_server()
         await app.initialize()
+        await post_init(app)
+        
         await app.start()
         await app.updater.start_polling()
 
